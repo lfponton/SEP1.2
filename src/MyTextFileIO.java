04f95287d8abf1a5573280bc511da4cec91f2e62
Luis <lfponton@gmail.com>
2020-12-16 19:39:38 +0100

    Delete unnecessary parameters in MyTextFileIO methods

--- src/MyTextFileIO.java
+++ src/MyTextFileIO.java
@@ -10,18 +10,18 @@ public class MyTextFileIO
    // Writes the given string to a file with the given file name
    public void writeToFile(String fileName, String str) throws FileNotFoundException
    {
-      write(fileName, str, false);  
+      write(fileName, str);
    }
    
    // writeToFile and appendToFile are almost identical - only the boolean in the constructor
    // of the FileOutputStream differs. So I made this private method that both methods call
-   private void write(String fileName, String str, boolean append) throws FileNotFoundException
+   private void write(String fileName, String str) throws FileNotFoundException
    {
       PrintWriter writeToFile = null;
 
       try
       {
-         FileOutputStream fileOutStream = new FileOutputStream(fileName, append);
+         FileOutputStream fileOutStream = new FileOutputStream(fileName);
          writeToFile = new PrintWriter(fileOutStream);
          writeToFile.println(str);
       }
